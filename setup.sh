b8ab726e70327a041c23da914cbad3f423bc25bc
David Reyinger <david@dreyinger.com>
2018-04-13 15:32:54 +0200

    add profileSetup implementation to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+RED='\033[0;31m'
+NC='\033[0m'
+
 gitConfig=false
 profileSetup=false
 
@@ -22,12 +25,8 @@ do
   esac
 done
 
-if [ $gitConfig = false ] && [ $profileSetup = false ]; then
-  echo "usage: setup.sh -git -profile"
-fi
-
 if [ $gitConfig = true ]; then
-  if [ ! which git -eq "" ]
+  if [ ! which git -eq "" ]; then
     git config --global alias.ci commit
     git config --global alias.co checkout
     git config --global alias.st status
@@ -35,4 +34,31 @@ if [ $gitConfig = true ]; then
     git config --global alias.lgs 'log --oneline --graph --decorate --stat'
     git config --global core.editor 'vim'
   fi
+fi
+
+if [ $profileSetup = true ]; then
+  if [ $HOME = "" ]; then
+    HOME = '~'
+  fi
+
+  # check for existing .profile
+  if [ -e $HOME/.profile ]; then
+    mv $HOME/.profile $HOME/.old_profile
+  fi
+
+  # check for existing zsh config
+  if [ -e $HOME/.zshrc ]; then
+    mv $HOME/.zshrc $HOME/.old_zshrc
+  fi
+
+  # check whethe zsh is installed
+  if [ ! which zsh -eq "" ]; then
+    echo -e "${RED}warning:\tno zsh installed${NC}"
+  fi
+
+  cp -r .profile $HOME/
+fi
+
+if [ $gitConfig = false ] && [ $profileSetup = false ]; then
+  echo "usage: setup.sh -git -profile"
 fi
